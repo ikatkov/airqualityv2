f7d816e46461dc7299c4d460003164573956eac0
Igor Katkov <ikatkov@atlassian.com>
2020-10-16 02:10:30 -0700

    Fixed precision loss

--- AQICalculator/AQICalculator.cpp
+++ AQICalculator/AQICalculator.cpp
@@ -11,7 +11,7 @@ AQICalculator:: AQICalculator()   = default;
 
 
 
-uint16_t correctValue(uint16_t value, uint8_t humidity) {
+double correctValue(uint16_t value, uint8_t humidity) {
     //this magic equation is from "PURPLEAIR PM2.5 U.S. CORRECTION AND PERFORMANCE DURING SMOKE EVENTS"
     //it corrects PMS5003 values to match US official
     return 0.534 * value - 0.0844 * humidity + 5.604;
@@ -26,7 +26,7 @@ int get_grid_index_(uint16_t value, uint16_t array[AMOUNT_OF_LEVELS][2]) {
     return -1;
 }
 
-int calculate_index_(uint16_t value, uint16_t array[AMOUNT_OF_LEVELS][2]) {
+double calculate_index_(double value, uint16_t array[AMOUNT_OF_LEVELS][2]) {
     uint16_t grid_index = get_grid_index_(value, array);
     if(grid_index == -1) return MAX_INDEX;
     uint16_t aqi_lo = index_grid_[grid_index][0];
@@ -34,15 +34,15 @@ int calculate_index_(uint16_t value, uint16_t array[AMOUNT_OF_LEVELS][2]) {
     uint16_t conc_lo = array[grid_index][0];
     uint16_t conc_hi = array[grid_index][1];
 
-    return ((aqi_hi - aqi_lo) / (conc_hi - conc_lo)) * (value - conc_lo) + aqi_lo;
+    return ((double)(aqi_hi - aqi_lo) / (conc_hi - conc_lo)) * (value - conc_lo) + aqi_lo;
 }
 
 
 uint16_t AQICalculator::getAqi(uint16_t pm2_5_value, uint16_t pm10_0_value, uint8_t humidity)  {
-    int pm2_5_index = calculate_index_(correctValue(pm2_5_value,humidity) , pm2_5_calculation_grid_);
-    int pm10_0_index = calculate_index_(correctValue(pm10_0_value,humidity), pm10_0_calculation_grid_);
-    Serial.println("pm2_5_index " + String(pm2_5_index));
-    Serial.println("pm10_0_index " + String(pm10_0_index));
+    uint16_t pm2_5_index = lround(calculate_index_(correctValue(pm2_5_value,humidity) , pm2_5_calculation_grid_));
+    uint16_t pm10_0_index = lround(calculate_index_(correctValue(pm10_0_value,humidity), pm10_0_calculation_grid_));
+   // Serial.println("pm2_5_index " + String(pm2_5_index));
+   // Serial.println("pm10_0_index " + String(pm10_0_index));
     return (pm2_5_index < pm10_0_index) ? pm10_0_index : pm2_5_index;
 }
 
